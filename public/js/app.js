2610ec5f702a4d558cf4240664f52f3895c76472
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-17 19:26:04 +0300

    tailwindcss built

--- public/js/app.js
+++ public/js/app.js
@@ -37319,6 +37319,17 @@ module.exports = function(module) {
 
 /***/ }),
 
+/***/ "./resources/css/main.css":
+/*!********************************!*\
+  !*** ./resources/css/main.css ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/js/app.js":
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
@@ -37387,14 +37398,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!**************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/css/main.css ***!
+  \**************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /Users/elijah/Desktop/Projects/MiniTwitterClone/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/elijah/Desktop/Projects/MiniTwitterClone/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/elijah/Desktop/Projects/MiniTwitterClone/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /Users/elijah/Desktop/Projects/MiniTwitterClone/resources/css/main.css */"./resources/css/main.css");
 
 
 /***/ })
